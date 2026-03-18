891b9f5ee724455fd9baac8ed1916100f6a6017b
Lucas Costa <lucasjpdc@gmail.com>
2026-03-18 14:37:41 -0300

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+Route::controller(SeriesController::class)->group(function () {
+    Route::get('/series', 'index');
+    Route::get('/series/criar', 'create');
+    Route::post('/series/salvar', 'store');
+});
