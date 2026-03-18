aa958164566bad5fb62633b0b09536c7812d2210
Lucas Costa <lucasjpdc@gmail.com>
2026-03-18 16:44:53 -0300

    Add series deletion feature and update web routes

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -3,7 +3,13 @@
 
 <ul class="list-group">
     @foreach ($series as $serie)
-    <li class="list-group-item">{{  $serie->nome }}</li>
+    <li class="list-group-item d-flex justify-content-between aligh-items-center">{{  $serie->nome }}
+        <form action="{{ route('series.destroy', $serie->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger btn-sm">X</button>
+        </form>
+    </li>
     @endforeach
 </ul>
 
